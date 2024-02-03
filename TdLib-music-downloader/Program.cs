472b1c2ad7a1e6e1e0466a46535c687b18e32e7d
UtileRain <utilerainpaw@gmail.com>
2024-02-03 18:48:35 +0500

    Create Auth functions in Program.cs (Based on TdSharp Sample)

--- TdLib-music-downloader/Program.cs
+++ TdLib-music-downloader/Program.cs
@@ -1,1 +1,128 @@
-﻿
+﻿using System;
+using TdLib;
+using TdLib.Bindings;
+using TdApi = TdLib.TdApi;
+
+internal static class TdLib_MusicDownloader
+{
+    private const int ApiId = 94575;
+    private const string ApiHash = "a3406de8d171bb422bb6ddf3bbd800e2";
+    private static string PhoneNumber = "";
+    private const string ApplicationVersion = "1.0.0";
+
+    private static TdClient _client;
+    private static readonly ManualResetEventSlim ReadyToAuthenticate = new();
+
+    private static bool _authNeeded;
+    private static bool _passwordNeeded;
+    private static bool _exit = false;
+
+    private static async Task Main()
+    {
+        _client = new TdClient();
+        // Creating Telegram client and setting minimal verbosity to Fatal since we don't need a lot of logs :)
+        _client = new TdClient();
+        _client.Bindings.SetLogVerbosityLevel(TdLogLevel.Fatal);
+
+        // Subscribing to all events
+        _client.UpdateReceived += async (_, update) => { await ProcessUpdates(update); };
+
+        // Waiting until we get enough events to be in 'authentication ready' state
+        ReadyToAuthenticate.Wait();
+
+        // We may not need to authenticate since TdLib persists session in 'td.binlog' file.
+        // See 'TdlibParameters' class for more information, or:
+        // https://core.telegram.org/tdlib/docs/classtd_1_1td__api_1_1tdlib_parameters.html
+        if (_authNeeded)
+        {
+            // Interactively handling authentication
+            await HandleAuthentication();
+        }
+
+        var currentUser = await _client.GetMeAsync();
+        var fullUserName = $"{currentUser.FirstName} {currentUser.LastName}".Trim();
+        Console.WriteLine($"Successfully logged in as [{currentUser.Id}] / [@{currentUser.Usernames?.ActiveUsernames[0]}] / [{fullUserName}]");
+    }
+
+    private static async Task HandleAuthentication()
+    {
+        // Setting phone number
+        await _client.ExecuteAsync(new TdApi.SetAuthenticationPhoneNumber
+        {
+            PhoneNumber = PhoneNumber
+        });
+
+        // Telegram servers will send code to us
+        Console.Write("Insert the login code: ");
+        var code = Console.ReadLine();
+
+        await _client.ExecuteAsync(new TdApi.CheckAuthenticationCode
+        {
+            Code = code
+        });
+
+        if (!_passwordNeeded) { return; }
+
+        // 2FA may be enabled. Cloud password is required in that case.
+        Console.Write("Insert the password: ");
+        var password = Console.ReadLine();
+
+        await _client.ExecuteAsync(new TdApi.CheckAuthenticationPassword
+        {
+            Password = password
+        });
+    }
+
+    private static async Task ProcessUpdates(TdApi.Update update)
+    {
+        // Since Tdlib was made to be used in GUI application we need to struggle a bit and catch required events to determine our state.
+        // Below you can find example of simple authentication handling.
+        // Please note that AuthorizationStateWaitOtherDeviceConfirmation is not implemented.
+
+        switch (update)
+        {
+            case TdApi.Update.UpdateAuthorizationState { AuthorizationState: TdApi.AuthorizationState.AuthorizationStateWaitTdlibParameters }:
+                // TdLib creates database in the current directory.
+                // so create separate directory and switch to that dir.
+                var filesLocation = Path.Combine(AppContext.BaseDirectory, "db");
+                await _client.ExecuteAsync(new TdApi.SetTdlibParameters
+                {
+                    ApiId = ApiId,
+                    ApiHash = ApiHash,
+                    DeviceModel = "PC",
+                    SystemLanguageCode = "en",
+                    ApplicationVersion = ApplicationVersion,
+                    DatabaseDirectory = filesLocation,
+                    FilesDirectory = filesLocation,
+                    // More parameters available!
+                });
+                break;
+
+            case TdApi.Update.UpdateAuthorizationState { AuthorizationState: TdApi.AuthorizationState.AuthorizationStateWaitPhoneNumber }:
+            case TdApi.Update.UpdateAuthorizationState { AuthorizationState: TdApi.AuthorizationState.AuthorizationStateWaitCode }:
+                _authNeeded = true;
+                ReadyToAuthenticate.Set();
+                break;
+
+            case TdApi.Update.UpdateAuthorizationState { AuthorizationState: TdApi.AuthorizationState.AuthorizationStateWaitPassword }:
+                _authNeeded = true;
+                _passwordNeeded = true;
+                ReadyToAuthenticate.Set();
+                break;
+
+            case TdApi.Update.UpdateUser:
+                ReadyToAuthenticate.Set();
+                break;
+
+            case TdApi.Update.UpdateConnectionState { State: TdApi.ConnectionState.ConnectionStateReady }:
+                // You may trigger additional event on connection state change
+                break;
+
+            default:
+                // ReSharper disable once EmptyStatement
+                ;
+                // Add a breakpoint here to see other events
+                break;
+        }
+    }
+}
