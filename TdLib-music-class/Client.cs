7576748d1849c418476aae14b4d2042e8e47712f
UtileRain <utilerainpaw@gmail.com>
2024-02-03 18:34:12 +0500

    Add Client.cs

---
+++ TdLib-music-class/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TdLib;
+using TdLib.Bindings;
+using TdApi = TdLib.TdApi;
+
+namespace TdLib_music_class
+{
+    public class Client
+    {
+        private TdClient _client;
+
+        public Client(TdLib.TdApi.SetTdlibParameters parameters) 
+        {
+            _client = new TdClient();
+            _client.Execute(parameters);
+        }
+
+    }
+}
